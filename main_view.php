332ddd3c94bb723e8ea9158add1ed409b6937776
Kooptikorn <58160506@go.buu.ac.th>
2017-11-04 22:52:45 +0700

    V2

--- main_view.php
+++ main_view.php
@@ -14,13 +14,13 @@
       <a class="navbar-brand" href="#">TheEyeHaveIt</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="#"><span class=" glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
     </ul>
   </div>
 </nav>
-  glskdgjxkdfjgldkjfdlgldfkg
+
 <div class="container">
 <h1>Search Product</h1>
 <label for="search">คำค้น:</label>
